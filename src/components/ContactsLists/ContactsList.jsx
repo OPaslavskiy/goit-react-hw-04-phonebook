9d5320830e13f25a598981c85452e9c880ba9c9d
OPaslavskiy <bankir.op@gmail.com>
2023-03-09 12:34:24 +0200

    all ready

--- src/components/ContactsLists/ContactsList.jsx
+++ src/components/ContactsLists/ContactsList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { List, Item, Btn, Name } from './ContactsList.styled';
 export const ContactsList = ({ contacts, deleteContact }) => {
   return (
@@ -15,15 +14,4 @@ export const ContactsList = ({ contacts, deleteContact }) => {
       ))}
     </List>
   );
-};
-
-ContactsList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  deleteContact: PropTypes.func.isRequired,
 };
--- src/components/FindContactImput/FindContactImput.jsx
+++ src/components/FindContactImput/FindContactImput.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import { Input, TitleForLabel, Lab } from './FindContactImput.styled';
-export const FindContactForm = ({ filter }) => {
+export const FindContactImput = ({ filter }) => {
   return (
     <Lab>
       <TitleForLabel>Find contacts by Name</TitleForLabel>
       <Input type="text" onChange={filter} />
     </Lab>
   );
-};
-
-FindContactForm.propTypes = {
-  filter: PropTypes.func.isRequired,
 };
